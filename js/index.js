e7eedfdfbf08266847f9c90325a8d9d42d3b2d20
Ricardo Soto <ri.soto.o@gmail.com>
2023-08-15 22:58:17 -0400

    Quitar css

--- js/index.js
+++ js/index.js
@@ -1,52 +1,41 @@
-// función para calcular el pago mensual del préstamo
-function calcularPago(monto, tasa, periodo) {
-    tasa = tasa / 100 / 12; // convertir la tasa anual a una tasa mensual
-    periodo = periodo * 12; // convertir el periodo en años a un periodo en meses
-  
-    // fórmula para calcular el pago mensual del préstamo
-    let pago = monto * tasa / (1 - Math.pow(1/(1 + tasa), periodo));
-  
-    return pago.toFixed(2); // redondear a 2 decimales
-  }
-  
-  // función para calcular el costo total del crédito
-  function calcularCostoTotal(pago, periodo) {
-    return (pago * periodo * 12).toFixed(2); // redondear a 2 decimales
-  }
-  
-  // inicio del simulador
-  document.getElementById("calcular").addEventListener("click", function() {
-    let tipo = document.getElementById("tipo").value;
-    let monto = document.getElementById("monto").value;
-    let periodo = document.getElementById("periodo").value;
-  
-    let tasa;
-    switch (tipo) {
-      case 'personal':
-        tasa = 20;
-        break;
-      case 'automotriz':
-        tasa = 2;
-        break;
-      case 'hipotecario':
-        tasa = 15;
-        break;
-    }
-  
-    let pago = calcularPago(monto, tasa, periodo);
-    let costoTotal = calcularCostoTotal(pago, periodo);
-    
-    document.getElementById("resultado").innerText = 
-      "Pago mensual: $" + pago + "\nCosto total del crédito: $" + costoTotal;
+document.addEventListener("DOMContentLoaded", function () {
+  const tipoPrestamoInput = document.getElementById("tipoPrestamo");
+  const montoInput = document.getElementById("monto");
+  const calcularButton = document.getElementById("calcular");
+  const resultadoDiv = document.getElementById("resultado");
+  const cuotaParagraph = document.getElementById("cuota");
+  const totalParagraph = document.getElementById("total");
+
+  calcularButton.addEventListener("click", function () {
+      const tipoPrestamo = tipoPrestamoInput.value;
+      const monto = parseFloat(montoInput.value);
+
+      let tasaAnual;
+      switch (tipoPrestamo) {
+          case "personal":
+              tasaAnual = 0.20;
+              break;
+          case "automotriz":
+              tasaAnual = 0.02;
+              break;
+          case "hipotecario":
+              tasaAnual = 0.15;
+              break;
+      }
+
+      const cuotaMensual = calcularCuotaMensual(monto, tasaAnual);
+      const costoTotal = cuotaMensual * 12 * 5; // Pago mensual por 5 años
+
+      cuotaParagraph.textContent = `Cuota Mensual: $${cuotaMensual.toFixed(2)}`;
+      totalParagraph.textContent = `Costo Total del Préstamo: $${costoTotal.toFixed(2)}`;
+
+      resultadoDiv.style.display = "block";
   });
-  
-  // inicio del envío de información
-  document.getElementById("enviar").addEventListener("click", function() {
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-  
-    // aquí iría el código para enviar la información al servidor o por correo electrónico
-  
-    console.log("Información enviada: ", nombre, email);
-  });
-  
+
+  function calcularCuotaMensual(monto, tasaAnual) {
+      const tasaMensual = tasaAnual / 12;
+      const plazoMeses = 60; // 5 años
+      const cuota = (monto * tasaMensual) / (1 - Math.pow(1 + tasaMensual, -plazoMeses));
+      return cuota;
+  }
+});
